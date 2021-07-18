c206d7644c7d6f556bd0a1d36f36b30787c8a544
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-07-18 20:17:12 +0000

    declare initial consts in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.getElementById('question');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progress-bar-full');
+const answers = Array.from(document.getElementsByClassName('answer-text'));
+const headerText = document.getElementById('header-text');
