531ec96c5980211f1d6ee6ba449e9b681f415ef4
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-07-18 11:21:50 +0000

    style game.css page and begin naming variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const  question = document.getElementById('question');
+const  choices = document.getElementsByClassName('choice-text');
+const  question = document.getElementById('question');
+const  question = document.getElementById('question');
+const  question = document.getElementById('question');
