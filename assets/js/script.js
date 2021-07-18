fb533a677fd62ebf00a16cd343922c0bb585f0e8
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-07-18 20:08:37 +0000

    save previous changes

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const  question = document.getElementById('question');
-const  choices = document.getElementsByClassName('choice-text');
-const  question = document.getElementById('question');
-const  question = document.getElementById('question');
-const  question = document.getElementById('question');
